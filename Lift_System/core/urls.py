bb7bead6bd18ac497ebd5da36c4680ec418ca157
krishna sharma <vaikrishna94@gmail.com>
2023-06-05 16:00:50 +0530

    database and simple APi

--- Lift_System/core/urls.py
+++ Lift_System/core/urls.py
@@ -1,10 +1,21 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from core.viewsets import ElevatorSystemViewSet
+from core.viewsets import ElevatorSystemViewSet, ElevatorRequestViewSet, ElevatorViewSet
+
 router = DefaultRouter()
 router.register(r'elevator-systems', ElevatorSystemViewSet)
 
 urlpatterns = [
+    path('lift-systems/<int:elevator_system>/elevators/<int:pk>/',
+         ElevatorViewSet.as_view({'patch': 'update'}), name='elevator-detail'),
+    path('lift-systems/<int:elevator_system>/elevators/<int:pk>/requests/',
+         ElevatorRequestViewSet.as_view({'get': 'requests_for_elevator'}), name='elevator-requests'),
+    path('lift-systems/<int:elevator_system>/elevators/<int:pk>/status/',
+         ElevatorViewSet.as_view({'get': 'status'}), name='status'),
+    path('lift-systems/<str:system_name>/', ElevatorSystemViewSet.as_view({'get': 'by_system_name'}),
+         name='elevator-systems-by-name'),
+    path('lift-systems/<int:pk>/create-elevator-request',
+         ElevatorSystemViewSet.as_view({'post': 'request_elevator'}), name='create-elevator-request'),
     path('', include(router.urls)),
 ]
--- Lift_System/core/viewsets/elevator_request_viewset.py
+++ Lift_System/core/viewsets/elevator_request_viewset.py
@@ -10,3 +10,12 @@ class ElevatorRequestViewSet(viewsets.ModelViewSet):
     queryset = ElevatorRequest.objects.all()
     serializer_class = ElevatorRequestSerializer
 
+    @action(detail=True, methods=['GET'])
+    def requests_for_elevator(self, request, pk=None, elevator_system=None):
+        try:
+            elevator = Elevator.objects.filter(elevator_system=elevator_system).get(pk=pk)
+            requests = self.queryset.filter(elevator=elevator)
+            serializer = self.get_serializer(requests, many=True)
+            return Response(serializer.data)
+        except Elevator.DoesNotExist:
+            return Response({'error': 'Elevator not found.'}, status=404)
